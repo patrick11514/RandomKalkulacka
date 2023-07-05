24aeef95d41f140917ebf187b0b7d03d9025c1b1
patrick11514 <ja@patrick115.eu>
2023-07-05 14:18:55 +0200

    Page of account

---
+++ src/routes/(main)/dash/account/[uuid]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+    import AccountSelector from '$components/accountSelector.svelte'
+</script>
+
+<svelte:head>
+    <title>{$page.params.uuid} | Finanční Kalkulačka</title>
+</svelte:head>
+
+<AccountSelector selected={$page.params.uuid} />
+
+{$page.params.uuid}
