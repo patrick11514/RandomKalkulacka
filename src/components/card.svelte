05bdd412ff29269060f8d07b26f4218e21eae20a
patrick11514 <ja@patrick115.eu>
2023-07-05 14:17:22 +0200

    Added tailwind merge

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+    import { twMerge } from 'tailwind-merge'
+
     let cls = ''
     export { cls as class }
 </script>
 
 <div class="flex min-h-screen">
     <div
-        class="m-auto py-10 px-2 min-w-[95%] sm:min-w-[80%] md:min-w-[55%] lg:min-w-[45%] xl:min-w-[40%] 2xl:min-w-[30%] bg-gradient-to-bl from-white to-slate-300 rounded-xl shadow-2xl text-black space-y-3 {cls}"
+        class={twMerge(
+            'm-auto py-10 px-2 min-w-[95%] sm:min-w-[80%] md:min-w-[55%] lg:min-w-[45%] xl:min-w-[40%] 2xl:min-w-[30%] bg-gradient-to-bl from-white to-slate-300 rounded-xl shadow-2xl text-black space-y-3',
+            cls
+        )}
     >
         <slot />
     </div>
